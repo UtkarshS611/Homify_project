ce8532011bcfcba7e3b7f4d87eb6425ac2a733cd
Utkarsh Singh <utkarsh.singh6113@gmail.com>
2024-05-20 20:50:20 +0530

    initial commit

---
+++ app/index.js
@@ -0,0 +1,12 @@
+const menu_close = document.querySelector('.menu_close');
+const menu_open = document.querySelector('.menu_open');
+const mobile_nav = document.querySelector('.mobile_nav');
+
+
+
+function abcd(){
+    mobile_nav.style.display = 'none';
+}
+function efgh(){
+    mobile_nav.style.display = 'flex';
+}
